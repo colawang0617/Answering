4872e720375ce3d46ddc5e8d4d99aee0bf6819c8
王颢泰 <wanghaotai@eduroam-campus-10-36-167-54.campus-dynamic.uwaterloo.ca>
2025-01-27 17:29:54 -0500

    modified the dropdown button

--- src/Components/Top.tsx
+++ src/Components/Top.tsx
@@ -1,31 +1,73 @@
 import Profile from "./Profile";
 import styles from "./Top.module.css"
+import { useEffect, useState, useRef } from "react";
 
 function Top(){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = {
+        all: useRef(null),
+        date: useRef(null),
+        type: useRef(null),
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                !dropdownRef.all.current ?.contains(event.target) &&
+                !dropdownRef.date.current ?.contains(event.target) && 
+                !dropdownRef.all.current ?.contains(event.target)
+            ) {
+                setIsOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const toggleDropdown = (dropdownName) => {
+        setIsOpen(isOpen === dropdownName ? false : dropdownName);
+    }
     return(
         <>
         <div className={styles.topcolumn}>
-            <div className={styles.dropdown}>
-                <button className={styles.dropbtn}>All</button>
-                <div className={styles.dropcnt}>
-                    <p>Pinned</p>
-                    <p>Favorite</p>
-                </div>
+            <div className={styles.dropdown} ref={dropdownRef.all}>
+                <button onClick={() => toggleDropdown("all")} className={styles.dropbtn}>
+                    All
+                </button>
+                {isOpen === "all" && (
+                    <div className={styles.dropcnt}>
+                        <button>Pinned</button>
+                        <button>Favorite</button>
+                    </div>
+                )}
             </div>
-            <div  className={styles.dropdown}>
-                <button className={styles.dropbtn}>Date</button>
-                <div className={styles.dropcnt}>
-                    <p>Date1</p>
-                    <p>Date2</p>
-                    <p>All</p>
-                </div>
+            <div  className={styles.dropdown} ref={dropdownRef}>
+                <button onClick={() => toggleDropdown("date")} className={styles.dropbtn}>
+                    Date
+                </button>
+                {isOpen === "date" && (
+                    <div className={styles.dropcnt}>
+                        <button>Date1</button>
+                        <button>Date2</button>
+                        <button>All</button>
+                    </div>
+                )}
             </div>
             <div className={styles.dropdown}>
-                <button className={styles.dropbtn}>AP</button>
-                <div className={styles.dropcnt}>
-                    <p>Honors</p>
-                    <p>Regular</p>
-                </div>
+                <button onClick={() => toggleDropdown("type")} className={styles.dropbtn}>
+                    Type
+                </button>
+                {isOpen === "type" && (
+                    <div className={styles.dropcnt}>
+                        <button>AP</button>
+                        <button>Honors</button>
+                        <button>Regular</button>
+                    </div>
+                )}
             </div>
             <input type="text" placeholder="Please Enter Your Search" className={styles.search}></input>
         </div>
