1a3aae41041c03fd65438da313717c0b0ac2f3a2
王颢泰 <wanghaotai@wanghaotaideMacBook-Air.local>
2025-03-13 23:50:10 -0400

    dropdown button improved

--- src/Components/SelectBar.tsx
+++ src/Components/SelectBar.tsx
@@ -1,21 +1,73 @@
+import { useState, useEffect,useRef } from 'react';
 import style from './Styles/SelectBar.module.css';
 import searchIcon from '../assets/search-icon.png';
 import arrow from '../assets/dropArrow.png';
 export default function SelectBar(){
+
+    const [isOpen, setIsOpen] = useState(false);
+    
+    const dropdownRef = {
+        Classes: useRef(null),
+        Level: useRef(null),
+        Date: useRef(null),
+    }
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (
+                !dropdownRef.Classes.current ?.contains(event.target) &&
+                !dropdownRef.Level.current ?.contains(event.target) &&
+                !dropdownRef.Date.current ?.contains(event.target)
+            ) {
+                    setIsOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const handleClick = (name) => {
+        if(isOpen === name){
+            setIsOpen(false);
+        }
+        else{
+            setIsOpen(name);
+        }
+    }
+
     return(
         <>
         <div className={style.selectBar}>
             <div className={style.leftbutton}>
-                <div className={style.kind}>
-                    <button className={style.kindButton1}>Classes</button>
+                <div className={style.kind} ref={dropdownRef.Classes}>
+                    <button className={style.kindButton1} onClick={() => handleClick("Classes")} >Classes</button>
+                    {isOpen === "Classes" && 
+                        <div className={style.dropbtn}>
+                           <button >All</button>
+                           <button>AP</button> 
+                           <button>Honors</button> 
+                           <button>Regular</button>  
+                        </div>}
                     <img src={arrow} alt='dropdown-Arrow' className={style.arrow}/>
                 </div>
-                <div className={style.kind}>
-                    <button className={style.kindButton}>Level</button>
+                <div className={style.kind} ref={dropdownRef.Level}>
+                    <button className={style.kindButton} onClick={() => handleClick("Level")}>Level</button>
+                    {isOpen === "Level" && 
+                        <div className={style.dropbtn}>
+                           <button>All</button>
+                           <button>AP</button> 
+                        </div>}
                     <img src={arrow} alt='dropdown-Arrow' className={style.arrow}/>
                 </div>
-                <div className={style.kind}>
-                    <button className={style.kindButton}>Date</button>
+                <div className={style.kind} ref={dropdownRef.Date}>
+                    <button className={style.kindButton} onClick={() => handleClick("Date")}>Date</button>
+                    {isOpen === "Date" && 
+                        <div className={style.dropbtn}>
+                           <button>All</button>
+                           <button>AP</button> 
+                           <button>Honors</button> 
+                        </div>}
                     <img src={arrow} alt='dropdown-Arrow' className={style.arrow}/>
                 </div>
             </div>
@@ -27,7 +79,6 @@ export default function SelectBar(){
                 <img src={searchIcon} alt='searchIcon' className={style.searchIcon}/>
             </div>
             <button>Reset</button>
-            
         </div>
         </>
     )
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,19 +5,21 @@ import style from "./Home.module.css";
 import courseData from "../courseData.js";
 import Ann from "../Components/Ann.js"
 
+
 export default function Home(){
 
     return(
         <>
             <div className={style.home}>
-                <Ann/>
                 <Header/>
                 <SelectBar/>
                 <div className={style.course}>
+                    <Ann/>
                     {courseData.map((courseData) => (
                         <Card1 key={courseData.id} id={courseData.id} image={courseData.image} course={courseData.course} subtitle={courseData.subtitle} />
-                    ))}
+                    ))} 
                 </div>
+                
             </div>
         </>
     );
